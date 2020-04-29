bbf2edde36700ad453fa8f8d4058680e9af84c52
feliannabila <56908379+feliannabila@users.noreply.github.com>
2020-04-30 04:57:11 +0700

    Add files via upload

---
+++ component/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0">
+		<title>COVID-19</title>
+				
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" href="<?php echo BASE_URL."css/style.css" ?>" type="text/css">
+	</head>
---
+++ component/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">COVID-19</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Data Kasus</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="https://github.com/feliannabila/UTS_PABW_FELIN">GitHub</a>
+      </li>
+    </ul>
+      <a href="logout.php" class="btn btn-outline-primary my-2 my-sm-0" type="submit">Logout</a>
+  </div>
+</nav>
